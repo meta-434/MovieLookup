6b0f6e2764e590c25960c704a64ca5e92b2fe7f5
Alex Hapgood <ach1@vt.edu>
2019-10-31 15:49:22 -0400

    NYT calls working
    look into refactoring all calls into async/await

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 const TMDB_API_KEY = "28c71925f5aad6fe8b7eb0161431ad96";
 const OMDB_API_KEY = "9c9b98e3";
+const NY_TIMES_API_KEY = "jIjbrgn4YdIHKu7cRx4EoufKwtsj7haJ";
+const TASTEFUL_API_KEY = "";
 
 /* makes calls to TMDB based on a keyword. TMDB is used for the initial
   search because it is much better at searching with incomplete titles and
@@ -16,12 +18,18 @@ function getMovies(srcParam) {
     })
     .then(responseJson => {
       console.log(responseJson);
-      displayResults(responseJson);
+      displaySearchResults(responseJson);
     })
     .catch(error => alert(`error: ${error}. search for ${srcParam} failed.`));
 }
 
-function getReviews (imdb_id) {
+async function getNytReviews (title, openYear) {
+  let response = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${title}&opening-date=${openYear}-01-01;${parseInt(openYear, 10) + 1}-01-01&api-key=${NY_TIMES_API_KEY}`);
+  let data = await response.json();
+  return data;
+}
+
+function getOmdbReviews (imdb_id) {
   fetch(`https://www.omdbapi.com/?i=${imdb_id}&apikey=${OMDB_API_KEY}`)
     .then(res => {
       if (res.ok) {
@@ -30,9 +38,9 @@ function getReviews (imdb_id) {
       throw new Error(res.statusText);
     })
     .then(resJson => {
-      console.log(resJson);
+      displayMovieInfo(resJson);
     })
-    .catch(error => alert(`error: ${error}.`))
+    .catch(error => alert(`error: ${error}. review search for ${imdb_id} failed.`))
 }
 
 function getImdbId (tmdb_id) {
@@ -44,12 +52,36 @@ function getImdbId (tmdb_id) {
       throw new Error(res.statusText);
     })
     .then(resJson => {
-      getReviews(resJson.imdb_id);
+      getOmdbReviews(resJson.imdb_id);
     })
-    .catch(error => alert(`error: ${error}`));
+    .catch(error => alert(`error: ${error}, imdb lookup for ${tmdb_id} failed.`));
 }
 
-function displayResults(json) {
+async function displayMovieInfo(json) {
+  const nytRev = await Promise.resolve(getNytReviews(json.Title, json.Year));
+
+  $('.results-movies').append(
+    `<div id="movie" data-imdb-id="${json.imdbID}" xmlns="http://www.w3.org/1999/html">
+            <h3>${json.Title} (${json.Year})<h3>
+            <img src=${json.Poster} alt="movie poster" />
+            <div>
+                <p>Released: ${json.Released}, DVD: ${json.DVD}</p>
+                <p>Starring: ${json.Actors}, Rated: ${json.Rated}</p>
+                <p>Director: ${json.Director} Genre(s): ${json.Genre}</p>
+                <p>Plot: ${json.Plot}</p>
+                <p>-Ratings-</p>
+                <ul>
+                    ${json.Ratings.map(rev => `<li>Source: ${rev.Source} Score: ${rev.Value}</li>`)}
+                </ul>
+                <p>-Reviews-</p>
+                <h4>${nytRev.results[0].headline} <br /> ${nytRev.results[0].byline}</h4>
+                <p>${nytRev.results[0].summary_short}</p>
+            </div>
+      </div>`
+  );
+}
+
+function displaySearchResults(json) {
   json.results.map(item => {
     const imgUrl = `http://image.tmdb.org/t/p/w342${item.poster_path}`;
     $('.results-movies').append(
